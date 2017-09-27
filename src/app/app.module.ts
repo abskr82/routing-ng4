493b99f5ffc815785722e248ff64500ff5e53f62
abskr82 <abhishekkumar647@outlook.com>
2017-09-27 23:38:46 +0530

    Adding example for anhular 4 routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, Component } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+import { Page3Component } from './page3/page3.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  {
+    path: '', component: Page1Component
+  },
+  {
+    path: 'page2', component: Page2Component
+  },
+  {
+    path: 'page3', component: Page3Component
+  },
+  {
+    path: 'not-found', component: PageNotFoundComponent
+  },
+  {
+    path: '**', redirectTo: '/not-found'
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Page1Component,
+    Page2Component,
+    Page3Component,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
